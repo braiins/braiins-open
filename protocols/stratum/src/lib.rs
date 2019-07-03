57cf06f87e32b1d3052a786f6562cfa97491ccd0
Jakub Horak <jakub.horak@braiins.com>
2019-07-03 13:03:58 +0200

    stratum: Add envlogger support
    Warning: this changed the default log level to `Error'

--- protocols/stratum/src/lib.rs
+++ protocols/stratum/src/lib.rs
@@ -2,7 +2,9 @@
 use lazy_static::lazy_static;
 use slog::{o, Drain, Level, LevelFilter, Logger};
 use slog_async;
+use slog_envlogger;
 use slog_term;
+use std::env;
 
 // TODO: is it best practice to reexport this?
 pub use packed_struct;
@@ -20,12 +22,19 @@ lazy_static! {
     /// Build static reference to the logger that will be accessible from all crates
     pub static ref LOGGER: Logger = {
         let level = Level::Trace;
+
+        // envlogger doesn't allow to set default log level, so this is a workaround
+        if !env::var("RUST_LOG").is_ok() {
+            env::set_var("RUST_LOG", "info");
+        }
+
         // Setup drain for terminal output
         let terminal_decorator = slog_term::TermDecorator::new().build();
         let terminal_drain = slog_term::FullFormat::new(terminal_decorator)
             .build()
             .fuse();
         let terminal_drain = LevelFilter::new(terminal_drain, level).fuse();
+        let terminal_drain = slog_envlogger::new(terminal_drain);
         let terminal_drain = slog_async::Async::new(terminal_drain).build().fuse();
 
         let log = Logger::root(terminal_drain, o!());
