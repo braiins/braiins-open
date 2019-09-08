818682eae335cccb4e9799638a0f48ff2e6a8bd4
Jan Čapek <jan@braiins.com>
2019-09-09 01:40:57 +0200

    stratum-sim: Allow detecting non-existing visitor method for a Message
    - implement custom exception VisitorMethodNotImplemented that denotes
      a missing visitor method
    
    - replace AttributError with Message.VisitorMethodNotImplemented and
      emit error message on the bus. The AttributeError was imprecise and
      was actually consuming any other potential runtime problems in the
      code.
    
    WIP:Message.VisitorMethodNotImplemented-squash

--- protocols/stratum/sim/sim_primitives/protocol.py
+++ protocols/stratum/sim/sim_primitives/protocol.py
@@ -7,16 +7,34 @@ from .network import Connection
 
 
 class Message:
-    """Generic message """
+    """Generic message that accepts visitors and dispatches their processing."""
+
+    class VisitorMethodNotImplemented(Exception):
+        """Custom handling to report if visitor method is missing"""
+
+        def __init__(self, method_name):
+            self.method_name = method_name
+
+        def __str__(self):
+            return self.method_name
 
     def __init__(self, req_id=None):
         self.req_id = req_id
 
     def accept(self, visitor):
         """Call visitor method based on the actual message type."""
-        getattr(visitor, 'visit_{}'.format(stringcase.snakecase(type(self).__name__)))(
-            visitor, self
-        )
+        method_name = 'visit_{}'.format(stringcase.snakecase(type(self).__name__))
+        try:
+            visit_method = getattr(visitor, method_name)
+        except AttributeError:
+            raise self.VisitorMethodNotImplemented(method_name)
+
+        visit_method(self)
+
+    def _format(self, content):
+        return '{}({})'.format(type(self).__name__, content)
+
+
 
 
 class ConnectionProcessor:
@@ -62,8 +80,12 @@ class ConnectionProcessor:
 
                 try:
                     msg.accept(self)
-                except AttributeError as e:
-                    self._on_invalid_message(msg)
+                except Message.VisitorMethodNotImplemented as e:
+                    self._emit_protocol_msg_on_bus(
+                        "{} doesn't implement:{}() for".format(type(self).__name_, e),
+                        msg,
+                    )
+                #    self._on_invalid_message(msg)
 
             except simpy.Interrupt:
                 self._emit_aux_msg_on_bus('DISCONNECTED')
