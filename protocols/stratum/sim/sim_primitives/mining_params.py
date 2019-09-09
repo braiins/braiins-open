90bc2cfab7bd16410f634b5f4d40b4f350cd59f8
Jan Čapek <jan@braiins.com>
2019-09-09 09:52:19 +0200

    stratum-sim: Add copyright headers

--- protocols/stratum/sim/sim_primitives/mining_params.py
+++ protocols/stratum/sim/sim_primitives/mining_params.py
@@ -1,3 +1,25 @@
+# Copyright (C) 2019  Braiins Systems s.r.o.
+#
+# This file is part of Braiins Open-Source Initiative (BOSI).
+#
+# BOSI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Please, keep in mind that we may also license BOSI or any part thereof
+# under a proprietary license. For more information on the terms and conditions
+# of such proprietary license or if you have any other questions, please
+# contact us at opensource@braiins.com.
+
 """This module gathers mining parameters"""
 
 diff_1_target = 0xFFFF << 208
