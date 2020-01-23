4c2861a54e83c99966b718edb6d4f7d956507e2a
Vojtech Kral <vojtech.kral@braiins.com>
2020-01-23 19:14:23 +0100

    wire: Add more data in AttemptError in Client

--- protocols/wire/src/client.rs
+++ protocols/wire/src/client.rs
@@ -88,14 +88,26 @@ impl Default for DefaultBackoff {
 }
 
 pub struct AttemptError<F: Framing> {
+    /// Duration since the last attempt when `next()` will
+    /// perform another retry
     pub next_attempt_in: Duration,
+    /// Number of failed reconnection attempts, including this one,
+    /// since the connection broke.
+    pub retries: u32,
+    /// The instant when the first re-connection attempt was started after the connection broke.
+    /// (You can use this to compute how long it has been in total since the connection broke
+    /// by subtracting this from `Instant::now()`)
+    pub error_time: Instant,
+    /// The I/O error returned by the underlying `Connection`.
     pub error: F::Error,
 }
 
 impl<F: Framing> AttemptError<F> {
-    fn new(next_attempt_in: Duration, error: F::Error) -> Self {
+    fn new(next_attempt_in: Duration, retries: u32, error_time: Instant, error: F::Error) -> Self {
         Self {
             next_attempt_in,
+            retries,
+            error_time,
             error,
         }
     }
@@ -106,6 +118,8 @@ pub struct Client<F: Framing> {
     addr: SocketAddr,
     backoff: Box<dyn Backoff>,
     next_delay: Option<(Instant, Duration)>,
+    retries: u32,
+    error_time: Option<Instant>,
     _marker: PhantomData<&'static F>,
 }
 
@@ -123,6 +137,8 @@ impl<F: Framing> Client<F> {
             addr,
             backoff: Box::new(backoff),
             next_delay: None,
+            retries: 0,
+            error_time: None,
             _marker: PhantomData,
         }
     }
@@ -136,6 +152,8 @@ impl<F: Framing> Client<F> {
     }
 
     pub async fn next(&mut self) -> Result<Connection<F>, AttemptError<F>> {
+        self.error_time.get_or_insert(Instant::now());
+
         if let Some((when, delay)) = self.next_delay.take() {
             let since_last_attempt = Instant::now().duration_since(when);
             if delay > since_last_attempt {
@@ -146,12 +164,16 @@ impl<F: Framing> Client<F> {
         match Connection::connect(&self.addr).await {
             Ok(conn) => {
                 self.backoff.reset();
+                self.retries = 0;
+                self.error_time = None;
                 Ok(conn)
             }
             Err(err) => {
                 let backoff = self.backoff.next();
                 self.next_delay = Some((Instant::now(), backoff));
-                Err(AttemptError::new(backoff, err))
+                self.retries += 1;
+                let error_time = self.error_time.unwrap();
+                Err(AttemptError::new(backoff, self.retries, error_time, err))
             }
         }
     }
