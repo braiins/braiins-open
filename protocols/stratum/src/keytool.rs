45a72d408a6ea441340b9bf70e5dac2e6067a938
Jan Čapek <jan@braiins.com>
2020-03-25 00:21:36 +0100

    stratum: v2: noise: Adjust keytool to support noise static keys
    - split gen-key command into two for generating:
    
      - Ed25519 keys for certification authority
      - Noise protocol specific static keys (e.g. x25519)
    
    - move `open_new_file()` and `write_to_file() to global namespace for
      sharing between both gen-*-key commands

--- protocols/stratum/src/keytool.rs
+++ protocols/stratum/src/keytool.rs
@@ -26,7 +26,6 @@
 //! - validating a specified certificate
 
 use anyhow::{anyhow, Context, Result};
-use ed25519_dalek::Keypair;
 use ii_stratum::v2::noise;
 use rand::rngs::OsRng;
 use std::convert::{TryFrom, TryInto};
@@ -43,74 +42,93 @@ use structopt::StructOpt;
     about = "Tool for generating ED25519 keypairs and certificates for Stratum V2 mining protocol"
 )]
 enum Command {
-    /// Generate keypair
-    GenKey(GenKeyCommand),
+    /// Generate CA keypair
+    GenCAKey(GenCAKeyCommand),
+    /// Generate Noise handshake keypair
+    GenNoiseKey(GenNoiseKeyCommand),
     /// Sign a specified key
     SignKey(SignKeyCommand),
 }
 
-/// Generates keypair and stores secret and public key into separate files
+/// Generates keypair suitable for certification authority and stores secret and public key into
+/// separate files
 #[derive(Debug, StructOpt)]
-struct GenKeyCommand {
-    #[structopt(short = "p", long, parse(from_os_str), default_value = "public.key")]
+struct GenCAKeyCommand {
+    #[structopt(
+        short = "p",
+        long,
+        parse(from_os_str),
+        default_value = "ca-ed25519-public.key"
+    )]
     public_key_file: PathBuf,
-    #[structopt(short = "s", long, parse(from_os_str), default_value = "secret.key")]
+    #[structopt(
+        short = "s",
+        long,
+        parse(from_os_str),
+        default_value = "ca-ed25519-secret.key"
+    )]
     secret_key_file: PathBuf,
 }
 
-impl GenKeyCommand {
-    fn open_new_file(file: &PathBuf, descr: &str) -> Result<File> {
-        OpenOptions::new()
-            .write(true)
-            .create_new(true)
-            .open(file)
-            .context(format!(
-                "cannot create {} ({:?})",
-                descr,
-                file.clone().into_os_string()
-            ))
-    }
-
-    fn write_to_file<T: TryInto<String>>(
-        file_path_buf: &PathBuf,
-        payload: T,
-        error_context_descr: &str,
-    ) -> Result<()>
-    where
-        T: TryInto<String>,
-        <T as std::convert::TryInto<std::string::String>>::Error: std::fmt::Display,
-    {
-        let mut file = Self::open_new_file(file_path_buf, error_context_descr)?;
-
-        let serialized_str: String = payload.try_into().map_err(|e| {
-            anyhow!(
-                "Cannot serialize {} ({:?}) {}",
-                error_context_descr,
-                file_path_buf,
-                e
-            )
-        })?;
-
-        file.write_all((serialized_str + "\n").as_bytes())?;
-
-        Ok(())
-    }
-
+impl GenCAKeyCommand {
     fn execute(self) -> Result<()> {
         print!("Generating ED25519 keypair...");
 
         let mut csprng = OsRng {};
-        let keypair: Keypair = Keypair::generate(&mut csprng);
+        let keypair = ed25519_dalek::Keypair::generate(&mut csprng);
 
-        Self::write_to_file(
+        write_to_file(
             &self.public_key_file,
             noise::auth::Ed25519PublicKeyFormat::new(keypair.public),
             "public key",
         )?;
-        Self::write_to_file(
+        write_to_file(
             &self.secret_key_file,
             noise::auth::Ed25519SecretKeyFormat::new(keypair.secret),
             "secret key",
+        )?;
+        println!("DONE");
+
+        Ok(())
+    }
+}
+
+/// Generates keypair suitable for use as 's' token (static key) in noise handshake.
+/// The output is stored into specified files.
+#[derive(Debug, StructOpt)]
+struct GenNoiseKeyCommand {
+    #[structopt(
+        short = "p",
+        long,
+        parse(from_os_str),
+        default_value = "server-noise-static-public.key"
+    )]
+    public_key_file: PathBuf,
+    #[structopt(
+        short = "s",
+        long,
+        parse(from_os_str),
+        default_value = "server-noise-static-secret.key"
+    )]
+    secret_key_file: PathBuf,
+}
+
+impl GenNoiseKeyCommand {
+    fn execute(self) -> Result<()> {
+        print!("Generating static ('s') keypair for Noise handshake ...");
+
+        let keypair = noise::generate_keypair()
+            .map_err(|e| anyhow!("Cannot generate noise keypair {}", e))?;
+
+        write_to_file(
+            &self.public_key_file,
+            noise::auth::StaticPublicKeyFormat::new(keypair.public),
+            "noise static public key",
+        )?;
+        write_to_file(
+            &self.secret_key_file,
+            noise::auth::StaticSecretKeyFormat::new(keypair.private),
+            "noise static secret key",
         )?;
         println!("DONE");
 
@@ -205,15 +223,57 @@ impl SignKeyCommand {
         let mut cert_file = self.public_key_to_sign.clone();
         cert_file.set_extension("cert");
 
-        GenKeyCommand::write_to_file(&cert_file, certificate, "certificate")
+        write_to_file(&cert_file, certificate, "certificate")
     }
+}
+
+/// Helper that opens a new file for writing or emits an error with specified context description
+/// if the file already exists. This is important to prevent overwriting already generated files.
+fn open_new_file(file: &PathBuf, descr: &str) -> Result<File> {
+    OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(file)
+        .context(format!(
+            "cannot create {} ({:?})",
+            descr,
+            file.clone().into_os_string()
+        ))
+}
+
+/// Helper that allows writing any String serializable type `payload` to be written into a
+/// specified path
+fn write_to_file<T: TryInto<String>>(
+    file_path_buf: &PathBuf,
+    payload: T,
+    error_context_descr: &str,
+) -> Result<()>
+where
+    T: TryInto<String>,
+    <T as std::convert::TryInto<std::string::String>>::Error: std::fmt::Display,
+{
+    let mut file = open_new_file(file_path_buf, error_context_descr)?;
+
+    let serialized_str: String = payload.try_into().map_err(|e| {
+        anyhow!(
+            "Cannot serialize {} ({:?}) {}",
+            error_context_descr,
+            file_path_buf,
+            e
+        )
+    })?;
+
+    file.write_all((serialized_str + "\n").as_bytes())?;
+
+    Ok(())
 }
 
 fn main() -> Result<()> {
     let command = Command::from_args();
 
     match command {
-        Command::GenKey(gen_key) => gen_key.execute(),
-        Command::SignKey(sign_key) => sign_key.execute(),
+        Command::GenCAKey(gen_key_cmd) => gen_key_cmd.execute(),
+        Command::GenNoiseKey(gen_key_cmd) => gen_key_cmd.execute(),
+        Command::SignKey(sign_key_cmd) => sign_key_cmd.execute(),
     }
 }
